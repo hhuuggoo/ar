4014f6dd366cae77a19b027174908ccc79f3f3f0
hugo <hugo.r.shi@gmail.com>
2014-10-08 18:52:00 -0400

    chunked rendering

--- render.py
+++ render.py
@@ -43,7 +43,10 @@ lxmin, lxmax, lymin, lymax = (-123., -70., -10., 50.)
 #lxmin, lxmax, lymin, lymax = xmin, xmax, ymin, ymax
 lxres = 600
 lyres = 400
-
+overlap = 3 #(in pixels)
+target_partitions = np.array([-123.1139268, -100.3898881,  -99.1809685,  -89.62     ,
+                              -75.6945583,  -74.61666  ,  -74.0758333,  -74.0758333,
+                              -73.120468 ,  -58.4583333])
 
 def nearest_scale(target, all_scales):
     if target > all_scales[-1]:
@@ -64,6 +67,7 @@ lbounds = (lxmin, lxmax, lymin, lymax)
 gbounds = (xmin, xmax, ymin, ymax)
 xscale, yscale = compute_scales(lbounds, gbounds, scales)
 
+
 def discrete_bounds(scales, lbounds, gbounds, lxres, lyres):
     xscale, yscale = scales
     lxmin, lxmax, lymin, lymax = lbounds
@@ -78,20 +82,56 @@ def discrete_bounds(scales, lbounds, gbounds, lxres, lyres):
     (gxmin, gymin) = (xmin, ymin)
 
     #local offsets
-    lxdim1 = (lxmin - gxmin) * gxgrid / (gxmax - gxmin)
-    lxdim2 = (lxmax - gxmin) * gxgrid / (gxmax - gxmin)
-    lydim1 = (lymin - gymin) * gygrid / (gymax - gymin)
-    lydim2 = (lymax - gymin) * gygrid / (gymax - gymin)
+    xunit = (gxmax - gxmin) / gxgrid
+    yunit = (gymax - gymin) / gygrid
+    lxdim1 = (lxmin - gxmin) / xunit
+    lxdim2 = (lxmax - gxmin) / xunit
+    lydim1 = (lymin - gymin) / yunit
+    lydim2 = (lymax - gymin) / yunit
     grid_shape = (gxgrid, gygrid)
     grid_data_bounds = (gxmin, gxmax, gymin, gymax)
-    local_indexes = (lxdim1, lxdim2, lydim1, lydim2)
-    return grid_shape, grid_data_bounds, local_indexes
+    local_indexes = (round(lxdim1), round(lxdim2), round(lydim1), round(lydim2))
 
-grid_shape, grid_data_bounds, local_indexes = discrete_bounds(
+    units = (xunit, yunit)
+    return grid_shape, grid_data_bounds, local_indexes, units
+
+grid_shape, grid_data_bounds, local_indexes, units = discrete_bounds(
     (xscale, yscale),
     lbounds,
     gbounds,
     lxres, lyres)
+
+def compute_partitions(target_partitions, gbounds, grid_shape, overlap):
+    xshape, yshape = grid_shape
+    (gxmin, gxmax, gymin, gymax) = gbounds
+    xlimit = xmax - xmin
+    xslope = xlimit / (xshape - 1)
+    xunit, yunit = units
+    chunks = []
+    for idx in range(0, len(target_partitions) - 1):
+        start = target_partitions[idx]
+        end = target_partitions[idx + 1]
+        start_idx = round((start - xmin) / xslope)
+        end_idx = round((end - xmin) / xslope)
+        start_idx_overlap = start_idx - overlap
+        if start_idx_overlap < 0:
+            start_idx_overlap = 0
+        end_idx_overlap = end_idx + overlap
+        if end_idx_overlap >= (xshape - 1):
+            end_idx_overlap = (xshape - 1)
+        start_val_overlap = xmin + xslope * start_idx_overlap
+        end_val_overlap = xmin + xslope * end_idx_overlap
+        start_val = xmin + xslope * start_idx
+        end_val = xmin + xslope * end_idx
+        chunk_info = (
+            overlap,
+            start_val, end_val,
+            start_val_overlap, end_val_overlap,
+            start_idx, end_idx,
+            start_idx_overlap, end_idx_overlap)
+        chunks.append(chunk_info)
+    return chunks
+
 
 ### dumb function that returns a circular-ish kernel for convolution
 def circle(radius=3):
@@ -137,16 +177,58 @@ def project(xdata, ydata, grid, xmin, xmax, ymin, ymax):
         if yoffset < yshape and xoffset < xshape:
             grid[xoffset, yoffset] += xrem * yrem
     return grid
-
 fast_project = numba.jit(project, nopython=True)
-st = time.time()
-output = fast_project(xdata, ydata,
-                      np.zeros(grid_shape),
-                      *grid_data_bounds)
-points = output
-output = scipy.ndimage.convolve(points, circle(radius=2))
+def render(xdata, ydata, grid, grid_data_bounds, shape):
+    output = fast_project(xdata, ydata,
+                          grid,
+                          *grid_data_bounds)
+    output = scipy.ndimage.convolve(output, shape)
+    return output
+
+class XChunkedGrid(object):
+    def __init__(self, data, yshape):
+        """data is a list of x offsets, and a 2d array
+        """
+        self.data = data
+        self.yshape = yshape
+
+    def get(self, xstart, xend):
+        bigdata = np.zeros(((xend - xstart), self.yshape))
+        xsize = xend - xstart
+        for x1, x2, data in self.data:
+            xx1 = max(x1, xstart)
+            xx2 = min(x2, xend)
+            if xx2 - xx1 > 0:
+                bigdata[xx1 - xstart:xx2 - xstart, :] = data[xx1 - x1:xx2 - x1, :]
+        return bigdata
+
+
+def render_chunked(xdata, ydata, chunks, grid_data_bounds, grid_shape, mark):
+    (gxmin, gxmax, gymin, gymax) = gbounds
+    data = []
+    for chunk_info in chunks:
+        (overlap, start_val, end_val,
+         start_val_overlap, end_val_overlap,
+         start_idx, end_idx,
+         start_idx_overlap, end_idx_overlap) = chunk_info
+        selector = [(xdata > start_val) & (xdata < end_val)]
+        xchunk = xdata[selector]
+        ychunk = ydata[selector]
+        grid = np.zeros((end_idx_overlap - start_idx_overlap, grid_shape[1]))
+        bounds = (start_val_overlap, end_val_overlap, gymin, gymax)
+        output = fast_project(xchunk, ychunk, grid, *bounds)
+        output = scipy.ndimage.convolve(output, mark)
+        st = start_idx - start_idx_overlap
+        ed = end_idx - start_idx_overlap
+        output = output[st:ed, :]
+        data.append((start_idx, end_idx, output))
+    return XChunkedGrid(data, grid_shape[-1])
+
+chunks = compute_partitions(target_partitions, gbounds, grid_shape, overlap)
+chunked = render_chunked(xdata, ydata, chunks, grid_data_bounds, grid_shape, circle(2))
 (lxdim1, lxdim2, lydim1, lydim2) = local_indexes
-output2 = output[lxdim1:lxdim2, lydim1:lydim2]
+output = chunked.get(0, grid_shape[0] - 1)
+output2 = chunked.get(lxdim1, lxdim2)[:, lydim1:lydim2]
 ed = time.time()
 print ed-st
 
@@ -162,3 +244,25 @@ pylab.imshow(output2.T[::-1,:] ** 0.2,
              cmap=cm.Greys_r,
              extent=lbounds,
              interpolation='nearest')
+
+#
+# st = time.time()
+# grid = np.zeros(grid_shape)
+# output = render(xdata, ydata, grid, grid_data_bounds, circle(2))
+# (lxdim1, lxdim2, lydim1, lydim2) = local_indexes
+# output2 = output[lxdim1:lxdim2, lydim1:lydim2]
+# ed = time.time()
+# print ed-st
+
+# ### after projecting individual points, convolve a shape(circle/square/cross)
+# ### over the points
+# import pylab
+# pylab.imshow(output.T[::-1,:] ** 0.2,
+#              cmap=cm.Greys_r,
+#              extent=grid_data_bounds,
+#              interpolation='nearest')
+# pylab.figure()
+# pylab.imshow(output2.T[::-1,:] ** 0.2,
+#              cmap=cm.Greys_r,
+#              extent=lbounds,
+#              interpolation='nearest')
