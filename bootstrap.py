bd50ea3981e3f4be4b0a01c923aa316b06da220d
hugo@continuum.io <hugo@continuum.io>
2014-10-28 17:37:31 -0400

    bootstrap dataset

--- bootstrap.py
+++ bootstrap.py
@@ -1,14 +1,8 @@
 import os
 from os.path import relpath, join, basename
 from kitchensink import setup_client, client, do, du, dp
-setup_client('http://power:6323/')
-c = client(rpc_name='data', queue_name='data')
-datadir = "/data"
-path = "/data/taxi"
-for root, dirs, files in os.walk(path):
-    for f in files:
-        path = join(root, f)
-        url = relpath(path, datadir)
-        c.bc('bootstrap', url, data_type='file', _queue_name='data|power2', _rpc_name='data')
+setup_client('http://localhost:6323/')
+c = client()
+c.bc('bootstrap', '/taxi/big.hdf5', data_type='file',_rpc_name='data')
 c.execute()
 c.br()
